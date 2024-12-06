c39868838dc8fbf3af93a0b2f621f63e073ebab3
Innes <innes.kwak@gmail.com>
2024-12-06 23:40:30 +0900

    feat : 회원가입 api 생성

--- src/apis/signUp.ts
+++ src/apis/signUp.ts
@@ -1,1 +1,7 @@
-// new file
+import { SignupRequest } from "../types/types";
+import api from "./axiosInstance";
+
+export const signUp = async (data: SignupRequest) => {
+  const response = await api.post("/register", data);
+  return response.data;
+};
