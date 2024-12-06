485089095db4647e89ab817cdf725a95defd99ee
Innes <innes.kwak@gmail.com>
2024-12-06 16:04:40 +0900

    setting : set react-router-dom, clear basic css, create main page and my page

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,25 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ["**/*.{ts,tsx}"],
+    ...tseslint.configs.recommended,
   },
-  pluginReact.configs.flat.recommended,
   {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    ...pluginReact.configs.flat.recommended,
     rules: {
-      "react/react-in-jsx-scope": "off", // 규칙 비활성화
+      "react/react-in-jsx-scope": "off",
     },
   },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "module" },
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
 ];
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import MyPage from "./pages/MyPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/mypage" element={<MyPage />} />
+    </Routes>
+  );
+}
 
 export default App;
