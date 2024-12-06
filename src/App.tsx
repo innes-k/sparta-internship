991895e8c5c8977b7beb01d71c5bd90478234e63
Innes <innes.kwak@gmail.com>
2024-12-07 02:07:41 +0900

    feat : 로그아웃 상태시 마이페이지 접근 불가 처리 (로그인 페이지로 대체)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useAuthStore } from "./store/authStore";
 import MainPage from "./pages/MainPage";
 import MyPage from "./pages/MyPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 
-function App() {
+const App = () => {
+  const { isLoggedIn } = useAuthStore();
+
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
-      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/mypage" element={isLoggedIn ? <MyPage /> : <Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
     </Routes>
   );
-}
+};
 
 export default App;
