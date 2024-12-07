b1398dc89fe951c7dc6cb9fcbb5052a1f074af92
Innes <innes.kwak@gmail.com>
2024-12-07 18:39:02 +0900

    feat : 회원정보 닉네임 수정 구현

--- src/apis/myPageApi.ts
+++ src/apis/myPageApi.ts
@@ -1,6 +1,13 @@
 import api from "./axiosInstance";
 
+import type { UpdateUserInfoRequest } from "../types/myPageTypes";
+
 export const getUserInfo = async () => {
   const response = await api.get("/user");
   return response.data;
+};
+
+export const updateUserInfo = async (newUserInfo: UpdateUserInfoRequest) => {
+  const response = await api.patch("/profile", newUserInfo);
+  return response;
 };
---
+++ src/types/myPageTypes.ts
@@ -0,0 +1,4 @@
+export interface UpdateUserInfoRequest {
+  avatar: string;
+  nickname: string;
+}
